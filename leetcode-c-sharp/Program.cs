a0f0f2a9b6a022726a2d743c648af617c9bacda1
gcpearse <glen.c.pearse@gmail.com>
2024-05-01 15:47:12 +0100

    updates switch statement

--- leetcode-c-sharp/Program.cs
+++ leetcode-c-sharp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace LeetCode
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length == 1)
+      {
+        switch (args[0])
+        {
+          case "1": TwoSum.Solution.Test(); break;
+
+          default: Console.WriteLine("Invalid argument."); break;
+        }
+      }
+      else
+      {
+        Console.WriteLine("Please provide a single argument.");
+      }
+    }
+  }
+}
