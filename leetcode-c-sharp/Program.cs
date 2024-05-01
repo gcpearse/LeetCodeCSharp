d4b687828198f21bda5e3ee853875035455e579b
gcpearse <glen.c.pearse@gmail.com>
2024-05-01 21:04:47 +0100

    converts to file-scoped namespace and moves switch statement into separate function

--- leetcode-c-sharp/Program.cs
+++ leetcode-c-sharp/Program.cs
@@ -1,25 +1,29 @@
-﻿namespace LeetCode
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      if (args.Length == 1)
-      {
-        switch (args[0])
-        {
-          case "1": TwoSum.Solution.Test(); break;
-          case "9": PalindromeNumber.Solution.Test(); break;
-          case "13": RomanToInteger.Solution.Test(); break;
-          case "14": LongestCommonPrefix.Solution.Test(); break;
+﻿namespace LeetCode;
 
-          default: Console.WriteLine("Invalid argument."); break;
-        }
-      }
-      else
-      {
-        Console.WriteLine("Please provide a single argument.");
-      }
+class Program
+{
+  static void Main(string[] args)
+  {
+    if (args.Length == 1)
+    {
+      TestSolution(args[0]);
+    }
+    else
+    {
+      Console.WriteLine("Please provide a single argument.");
+    }
+  }
+
+  static void TestSolution(string arg)
+  {
+    switch (arg)
+    {
+      case "1": TwoSum.Solution.Test(); break;
+      case "9": PalindromeNumber.Solution.Test(); break;
+      case "13": RomanToInteger.Solution.Test(); break;
+      case "14": LongestCommonPrefix.Solution.Test(); break;
+
+      default: Console.WriteLine("Invalid argument."); break;
     }
   }
 }
