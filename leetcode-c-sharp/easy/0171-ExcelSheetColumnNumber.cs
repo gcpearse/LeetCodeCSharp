74b8b4c6b6a3bdcf515598b26761e78ea062fca6
gcpearse <glen.c.pearse@gmail.com>
2024-05-07 09:24:58 +0100

    adds tested solution

---
+++ leetcode-c-sharp/easy/0171-ExcelSheetColumnNumber.cs
@@ -0,0 +1,62 @@
+namespace ExcelSheetColumnNumber;
+
+public class Solution
+{
+  public static int TitleToNumber(string columnTitle)
+  {
+    int result = 0;
+    int multiplier = 1;
+
+    for (int i = columnTitle.Length - 1; i >= 0 ; i--)
+    {
+      result += (columnTitle[i] - 64) * multiplier;
+      multiplier *= 26;
+    }
+
+    return result;
+  }
+
+  public static void Test()
+  {
+    Console.WriteLine(TitleToNumber("A"));
+    // 1
+    Console.WriteLine(TitleToNumber("AB"));
+    // 28
+    Console.WriteLine(TitleToNumber("ZY"));
+    // 701
+  }
+}
+
+
+/*
+
+Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+Example 1:
+
+Input: columnTitle = "A"
+Output: 1
+
+Example 2:
+
+Input: columnTitle = "AB"
+Output: 28
+
+Example 3:
+
+Input: columnTitle = "ZY"
+Output: 701
+
+*/
