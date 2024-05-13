dfe26945ba9a06d88caddfb8f13b7d80aa09d84f
gcpearse <glen.c.pearse@gmail.com>
2024-05-13 13:20:36 +0100

    adds tested solution

---
+++ leetcode-c-sharp/easy/0392-IsSubsequence.cs
@@ -0,0 +1,58 @@
+namespace IsSubsequence;
+
+public class Solution
+{
+  public static bool IsSubsequence(string s, string t)
+  {
+    if (s.Length == 0) return true;
+
+    int count = s.Length;
+    int index = 0;
+
+    for (int i = 0; i < t.Length; i++)
+    {
+      if (t[i] == s[index])
+      {
+        count--;
+        
+        if (index + 1 < s.Length)
+        {
+          index++;
+        }
+      }
+    }
+
+    return count == 0;
+  }
+
+  public static void Test()
+  {
+    Console.WriteLine(IsSubsequence("abc", "ahbgdc"));
+    // True
+    Console.WriteLine(IsSubsequence("axc", "ahbgdc"));
+    // False
+    Console.WriteLine(IsSubsequence("acb", "ahbgdc"));
+    // False
+    Console.WriteLine(IsSubsequence("b", "abc"));
+    // True
+  }
+}
+
+
+/*
+
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+*/
