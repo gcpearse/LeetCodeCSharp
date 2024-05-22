c2e18d892df2591c9b1d2b3c16f8fed607f0178b
gcpearse <glen.c.pearse@gmail.com>
2024-05-22 17:13:47 +0100

    adds tested solution

---
+++ leetcode-c-sharp/easy/0482-LicenseKeyFormatting.cs
@@ -0,0 +1,59 @@
+namespace LicenseKeyFormatting;
+
+public class Solution
+{
+  public static string LicenseKeyFormatting(string s, int k)
+  {
+    int count = k;
+    string str = s.Replace("-", "");
+    List<char> chars = [];
+
+    for (int i = str.Length - 1; i >= 0; i--)
+    {
+      chars.Insert(0, char.ToUpper(str[i]));
+      count--;
+
+      if (count == 0 && i > 0)
+      {
+        chars.Insert(0, '-');
+        count = k;
+      }
+    }
+
+    return new string(chars.ToArray());
+  }
+
+  public static void Test()
+  {
+    Console.WriteLine(LicenseKeyFormatting("5F3Z-2e-9-w", 4));
+    // 5F3Z-2E9W
+    Console.WriteLine(LicenseKeyFormatting("2-5g-3-J", 2));
+    // 2-5G-3J
+    Console.WriteLine(LicenseKeyFormatting("--a-a-a-a--", 2));
+    // AA-AA
+  }
+}
+
+
+/*
+
+You are given a license key represented as a string s that consists of only alphanumeric characters and dashes. The string is separated into n + 1 groups by n dashes. You are also given an integer k.
+
+We want to reformat the string s such that each group contains exactly k characters, except for the first group, which could be shorter than k but still must contain at least one character. Furthermore, there must be a dash inserted between two groups, and you should convert all lowercase letters to uppercase.
+
+Return the reformatted license key.
+
+Example 1:
+
+Input: s = "5F3Z-2e-9-w", k = 4
+Output: "5F3Z-2E9W"
+Explanation: The string s has been split into two parts, each part has 4 characters.
+Note that the two extra dashes are not needed and can be removed.
+
+Example 2:
+
+Input: s = "2-5g-3-J", k = 2
+Output: "2-5G-3J"
+Explanation: The string s has been split into three parts, each part has 2 characters except the first part as it could be shorter as mentioned above.
+
+*/
