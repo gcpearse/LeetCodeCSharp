8bc62fe4060665b73dc84c16a0cbbe2825777f86
gcpearse <glen.c.pearse@gmail.com>
2024-05-23 14:09:02 +0100

    adds tested solution

---
+++ leetcode-c-sharp/easy/0495-TeemoAttacking.cs
@@ -0,0 +1,60 @@
+namespace TeemoAttacking;
+
+public class Solution
+{
+  public static int FindPoisonedDuration(int[] timeSeries, int duration)
+  {
+    int count = 0;
+
+    for (int i = 0; i < timeSeries.Length - 1; i++)
+    {
+      if (timeSeries[i + 1] - timeSeries[i] > duration)
+      {
+        count += duration;
+      }
+      else
+      {
+        count += timeSeries[i + 1] - timeSeries[i];
+      }
+    }
+
+    return count + duration;
+  }
+
+  public static void Test()
+  {
+    Console.WriteLine(FindPoisonedDuration([1, 4], 2));
+    // 4
+    Console.WriteLine(FindPoisonedDuration([1, 2], 2));
+    // 3
+  }
+}
+
+
+/*
+
+Our hero Teemo is attacking an enemy Ashe with poison attacks! When Teemo attacks Ashe, Ashe gets poisoned for a exactly duration seconds. More formally, an attack at second t will mean Ashe is poisoned during the inclusive time interval [t, t + duration - 1]. If Teemo attacks again before the poison effect ends, the timer for it is reset, and the poison effect will end duration seconds after the new attack.
+
+You are given a non-decreasing integer array timeSeries, where timeSeries[i] denotes that Teemo attacks Ashe at second timeSeries[i], and an integer duration.
+
+Return the total number of seconds that Ashe is poisoned.
+
+Example 1:
+
+Input: timeSeries = [1, 4], duration = 2
+Output: 4
+Explanation: Teemo's attacks on Ashe go as follows:
+- At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
+- At second 4, Teemo attacks, and Ashe is poisoned for seconds 4 and 5.
+Ashe is poisoned for seconds 1, 2, 4, and 5, which is 4 seconds in total.
+
+Example 2:
+
+Input: timeSeries = [1, 2], duration = 2
+Output: 3
+Explanation: Teemo's attacks on Ashe go as follows:
+- At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
+- At second 2 however, Teemo attacks again and resets the poison timer. Ashe is poisoned for seconds 2 and 3.
+Ashe is poisoned for seconds 1, 2, and 3, which is 3 seconds in total.
+
+*/
